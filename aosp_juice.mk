3c06b701fe2c5408023d2285413b444d18e4b13e
imannig <akugedekntl@gmail.com>
2022-02-27 01:14:44 +0700

    Lets go Elixir official

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -8,7 +8,6 @@ $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_GAPPS_ARCH := arm64
 
 # Bootanimation
@@ -27,4 +26,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
 
 # Elixir additions
-Elixir_MAINTAINER := Ansaria
+IS_PHONE := true
+TARGET_SUPPORTS_QUICK_TAP := false
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
